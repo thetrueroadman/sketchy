756baf2c59d559b2e8cffd46b459cfd5c2df22c8
Rort <thetrueroadman@gmail.com>
2022-11-26 12:55:57 -0500

    Divs now stay black

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const grid = document.querySelector('#grid');
+const grid = document.querySelector("#grid");
 let gridWidth = 8;
 for (let i = 0; i < (gridWidth * gridWidth); i++) {
   const square = document.createElement("div");
+  square.className = "squareDiv";
+  square.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor = 'black';
+  })
   square.innerHTML = "cup";
   grid.appendChild(square);
 }
