eeb5cd15c97fe3f24e19907b858ed46448ec367e
Rort <thetrueroadman@gmail.com>
2022-12-02 13:00:54 -0500

    Added reset functionality to reset button.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const grid = document.querySelector("#grid");
 let gridWidth = 8;
-for (let i = 0; i < (gridWidth * gridWidth); i++) {
-  const square = document.createElement("div");
-  square.className = "squareDiv";
-  square.addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = 'black';
-  })
-  square.innerHTML = "cup";
-  grid.appendChild(square);
+function createGrid() {
+  for (let i = 0; i < (gridWidth * gridWidth); i++) {
+    const square = document.createElement("div");
+    square.className = "squareDiv";
+    square.addEventListener("mouseover", function(event) {
+      event.target.style.backgroundColor = 'black';
+    })
+    square.innerHTML = "cup";
+    grid.appendChild(square);
+  }
 }
+
+createGrid();
+
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", e => {
+  let elements = document.getElementsByClassName("squareDiv");
+  while(elements.length > 0){
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+  createGrid();
+})
